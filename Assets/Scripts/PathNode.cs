c50126d7ab2a949d95b1a32c60e60337f97de04c
Emil frederiksen <Frederiksen_33@hotmail.com>
2020-06-09 09:21:33 +0200

    Added isWalkable bool made x,y pulic for acess int the Pathfinding class

--- Assets/Scripts/PathNode.cs
+++ Assets/Scripts/PathNode.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class PathNode : MonoBehaviour
 {
     private Grid<PathNode> grid;
-    private int x;
-    private int y;
+    public int x;
+    public int y;
 
     public int gCost;
     public int hCost;
     public int fCost;
 
+    public bool isWalkable;
     public PathNode previousNode;
 
     public PathNode(Grid<PathNode> grid, int x, int y)
@@ -19,6 +20,7 @@ public class PathNode : MonoBehaviour
         this.grid = grid;
         this.x = x;
         this.y = y;
+        this.isWalkable = true;
     }
 
     public void CalculateFcost()
